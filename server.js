164d3ac6117df5a176a83750520ded19943bdab3
Denisse Costilla <122069291+giniara@users.noreply.github.com>
2025-01-09 23:51:58 +0000

    configuración de servidor

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+console.log('Express cargado correctamente');
+
+const { graphqlHTTP } = require('express-graphql');
+console.log('express-graphql cargado correctamente');
+
+
+const { buildSchema } = require('graphql');
+
+// Definir el esquema GraphQL
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// Definir los resolvers
+const root = {
+  hello: () => {
+    return 'Hello, world!';
+  },
+};
+
+const app = express();
+
+// Ruta para la raíz
+app.get('/', (req,res) => {
+        res.send('Bienvenido al servidor GraphQL. Visita /graphql para usar GraphQL.');
+});
+
+// Configurar la ruta /graphql
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(4000, () => {
+  console.log('Servidor GraphQL ejecutándose en http://localhost:4000/graphql');
+});
